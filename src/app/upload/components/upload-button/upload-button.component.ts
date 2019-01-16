0af443b88a951f0c5b1fdde2b6d5d34c2d21db99
0x6e68 <nico.hartma@gmail.com>
2019-01-16 16:12:46 +0100

    restructure; add angular material

---
+++ src/app/upload/components/upload-button/upload-button.component.ts
@@ -0,0 +1,37 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {DataNodeService} from "../../../services/data-node.service";
+
+@Component({
+  selector: 'app-upload-button',
+  templateUrl: './upload-button.component.html',
+  styleUrls: ['./upload-button.component.scss']
+})
+export class UploadButtonComponent implements OnInit {
+
+  @Input()
+  dataBlob: Blob;
+
+  constructor(private dataNodeService: DataNodeService) { }
+
+  ngOnInit() {
+  }
+
+  postDataTransaction(){
+    if(this.dataBlob){
+
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const metaData = {
+          type: this.dataBlob.type
+        };
+
+        const result = reader.result as ArrayBuffer;
+        this.dataNodeService.postDataTransaction(result,metaData);
+
+      };
+
+      reader.readAsArrayBuffer(this.dataBlob);
+    }
+  }
+}
