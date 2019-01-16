49280816f64b58e5333bea27e15842b13c831cec
0x6e68 <nico.hartma@gmail.com>
2019-01-16 16:09:54 +0100

    restructure; add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {TransactionListComponent} from "./transaction-list/transaction-list.component";
 import {DataNodeService} from "./services/data-node.service";
 import {Web3Service} from "./services/web3.service";
@@ -12,11 +12,15 @@ import {TransactionListItem} from "./transaction-list-item/transaction-list-item
 import {UploadFileComponent} from "./upload-file-form/upload-file-form.component";
 import {FileDropModule} from "ngx-file-drop";
 import {ItemImageDataComponent} from "./transaction-list-item/item-data/image/item-image-data.component";
-import { MessageInputComponent } from './upload/components/message-input/message-input.component';
-import { UploadCostInfoComponent } from './upload/components/upload-cost-info/upload-cost-info.component';
+import {MessageInputComponent} from './upload/components/message-input/message-input.component';
+import {UploadCostInfoComponent} from './upload/components/upload-cost-info/upload-cost-info.component';
+import {TransactionCostInfoComponent} from './upload/components/transaction-cost-info/transaction-cost-info.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { TransactionCostInfoComponent } from './upload/components/transaction-cost-info/transaction-cost-info.component';
+import { FileInputComponent } from './upload/components/file-input/file-input.component';
+import { UploadButtonComponent } from './upload/components/upload-button/upload-button.component';
+import {MatCardModule, MatGridListModule, MatInputModule, MatTabsModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -30,13 +34,20 @@ import { TransactionCostInfoComponent } from './upload/components/transaction-co
     MessageInputComponent,
     UploadCostInfoComponent,
     TransactionCostInfoComponent,
+    FileInputComponent,
+    UploadButtonComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FileDropModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTabsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule
   ],
   providers: [
     DataNodeService,
@@ -44,4 +55,5 @@ import { TransactionCostInfoComponent } from './upload/components/transaction-co
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
