b08e7822bc0532ff1d5487b0fbe9558e4e124bb2
0x6e68 <nico.hartma@gmail.com>
2019-02-21 00:42:12 +0100

    remove unnessecary firebase configs & libs; remove origin address restriction for master

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,14 +3,7 @@ export const environment = {
   contract: {
     defaultAddress: '0xd07B5daCB15B931daC559a36E0aB67f433ccF7a6',
     searchFromBlockNumber: 7229555,
-    onlyLoadDataWithOriginAddress: '0x2c561E032ce839dE9f7199A7E633a339275c9566'
+    onlyLoadDataWithOriginAddress: null
   },
-  firebase: {
-    apiKey: "AIzaSyCJkRF2j2hTJG6NVq893mzF7adCkV6Dlw0",
-    authDomain: "datanode-4d7ae.firebaseapp.com",
-    databaseURL: "https://datanode-4d7ae.firebaseio.com",
-    projectId: "datanode-4d7ae",
-    storageBucket: "datanode-4d7ae.appspot.com",
-    messagingSenderId: "516062328295"
-  }
+
 };
