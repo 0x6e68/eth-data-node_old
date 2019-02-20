cf2ca48cbad7060cd63453d23f77a2872657349b
stoney1989 <nico.hartma@gmail.com>
2019-02-20 23:22:55 +0100

    clean up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  defaultContractAddress: '0xd07B5daCB15B931daC559a36E0aB67f433ccF7a6',
+  firebase: {
+    apiKey: "AIzaSyCJkRF2j2hTJG6NVq893mzF7adCkV6Dlw0",
+    authDomain: "datanode-4d7ae.firebaseapp.com",
+    databaseURL: "https://datanode-4d7ae.firebaseio.com",
+    projectId: "datanode-4d7ae",
+    storageBucket: "datanode-4d7ae.appspot.com",
+    messagingSenderId: "516062328295"
+  }
 };
 
 /*
