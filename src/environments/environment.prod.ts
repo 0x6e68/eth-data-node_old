c56f1f16707a3109f3fdf333dae5a4ed2cd85d90
0x6e68 <nico.hartma@gmail.com>
2019-02-20 23:22:55 +0100

    clean up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  defaultContractAddress: '0xd07B5daCB15B931daC559a36E0aB67f433ccF7a6',
+  firebase: {
+    apiKey: "AIzaSyCJkRF2j2hTJG6NVq893mzF7adCkV6Dlw0",
+    authDomain: "datanode-4d7ae.firebaseapp.com",
+    databaseURL: "https://datanode-4d7ae.firebaseio.com",
+    projectId: "datanode-4d7ae",
+    storageBucket: "datanode-4d7ae.appspot.com",
+    messagingSenderId: "516062328295"
+  }
 };
