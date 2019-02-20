0db3feab76cd9fabe28613732a113f02571c4d47
stoney1989 <nico.hartma@gmail.com>
2019-02-21 00:44:08 +0100

    add demo restrictions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   contract: {
     defaultAddress: '0xd07B5daCB15B931daC559a36E0aB67f433ccF7a6',
     searchFromBlockNumber: 7229555,
-    onlyLoadDataWithOriginAddress: null
+    onlyLoadDataWithOriginAddress: '0x2c561E032ce839dE9f7199A7E633a339275c9566'
   },
 
 
