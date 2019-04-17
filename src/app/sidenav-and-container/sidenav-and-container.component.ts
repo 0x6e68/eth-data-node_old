60222396b954cd41207f968c454a1e0cc3c361c7
stoney1989 <nico.hartma@gmail.com>
2019-04-17 16:20:23 +0200

    move side nav & content into a component; add toggle switch for side navigation

---
+++ src/app/sidenav-and-container/sidenav-and-container.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {WEB3_SERVICE_STATE, Web3Service} from "../services/web3.service";
+
+@Component({
+  selector: 'app-sidenav-and-container',
+  templateUrl: './sidenav-and-container.component.html',
+  styleUrls: ['./sidenav-and-container.component.scss']
+})
+export class SidenavAndContainerComponent implements OnInit {
+  WEB3_SERVICE_STATE = WEB3_SERVICE_STATE;
+  title = 'data-node';
+
+  showSideNav: boolean = true;
+
+  constructor(private web3Service: Web3Service) {
+  }
+
+  ngOnInit() {
+  }
+
+  getWeb3State(): WEB3_SERVICE_STATE {
+    return this.web3Service.state;
+  }
+
+  toggleShowHideSideNav(){
+    this.showSideNav = !this.showSideNav;
+  }
+
+}
